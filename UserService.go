4cb8d3bfd79ef74c484f51c71ac7ed88fff1f2c2
Shskjssk10 <cadentohjunyi@gmail.com>
2024-12-01 18:27:10 +0800

    Setting up database connection

--- UserService.go
+++ UserService.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 var port int = 8000
@@ -16,4 +22,56 @@ type User struct {
 	MembershipTier string
 	DateJoined     time.Time
 	asswordHash    string
+}
+
+var db *sql.DB
+
+func connectToDB() (*sql.DB, error) {
+	if db != nil {
+		// Check if the database connection is already established
+		err := db.Ping()
+		if err == nil {
+			return db, nil
+		}
+	}
+
+	// If not connected or there's an error, establish a new connection
+	db, err := sql.Open("mysql", "root:Shskjssk10!@tcp(127.0.0.1:3306)/CNADAssg1DB")
+	if err != nil {
+		log.Fatal("Error connecting to the database:", err)
+		return nil, err
+	}
+
+	// Ping the database to ensure the connection is successful
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Error pinging the database:", err)
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	// Routes
+	router.HandleFunc("/api/v1/test", testingDB).Methods("GET")
+
+	corsHandler := handlers.CORS(handlers.AllowedOrigins([]string{"http://127.0.0.1:8000"}))(router)
+
+	fmt.Printf("Listening at port %d\n", port)
+	url := fmt.Sprintf(":%d", port)
+	log.Fatal(http.ListenAndServe(url, corsHandler))
+}
+
+func testingDB(w http.ResponseWriter, r *http.Request) {
+	db, err := connectToDB()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		fmt.Println("Error connecting to the database")
+		return
+	}
+	fmt.Println(db)
+	defer db.Close()
 }
