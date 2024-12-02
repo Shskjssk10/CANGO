2b1dd0ffb67c062a65efe637dc1c0f39137fc1ae
Shskjssk10 <133301405+Shskjssk10@users.noreply.github.com>
2024-12-02 13:50:52 +0800

    Implemented Hashing and registerUser function

--- UserService.go
+++ UserService.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var port int = 8000
@@ -20,7 +21,6 @@ type User struct {
 	EmailAddr      string
 	ContactNo      string
 	MembershipTier string
-	DateJoined     time.Time
 	PasswordHash   string
 }
 
@@ -58,11 +58,54 @@ func main() {
 	// Routes
 	router.HandleFunc("/api/v1/test", testingDB).Methods("GET")
 
-	corsHandler := handlers.CORS(handlers.AllowedOrigins([]string{"http://127.0.0.1:8000"}))(router)
+	router.HandleFunc("/api/v1/registerUser", registerUser).Methods("POST")
+
+	corsHandler := handlers.CORS(
+		handlers.AllowedOrigins([]string{"http://127.0.0.1:8000"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS", "PUT"}),
+	)(router)
 
 	fmt.Printf("Listening at port %d\n", port)
 	url := fmt.Sprintf(":%d", port)
 	log.Fatal(http.ListenAndServe(url, corsHandler))
+}
+
+func registerUser(w http.ResponseWriter, r *http.Request) {
+	db, err := connectToDB()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		fmt.Println("Error connecting to the database")
+		return
+	}
+
+	var newUser User
+	err = json.NewDecoder(r.Body).Decode(&newUser)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Hash password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newUser.PasswordHash), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, "Failed to hash password", http.StatusInternalServerError)
+		return
+	}
+
+	_, err = db.Exec(`
+		INSERT INTO User (Name, EmailAddr, ContactNo, PasswordHash)
+		VALUES 
+		(?, ?, ?, ?)`, newUser.Name, newUser.EmailAddr, newUser.ContactNo, hashedPassword)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		fmt.Println("Something went wrong with creation")
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode("User Successully Created!")
+
+	w.WriteHeader(http.StatusAccepted)
 }
 
 func testingDB(w http.ResponseWriter, r *http.Request) {
